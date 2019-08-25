686b532d5838c787798fa418c042750f31171501
mubeen302 <45149254+mubeen302@users.noreply.github.com>
2019-08-26 00:25:26 +0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+$('.open-menu-icon').on('click',function(){
+    $('.main-nav-outer').addClass('open-menu');
+});
+
+$('.menu-close-icon').click(function(){
+    $('.main-nav-outer').removeClass('open-menu');
+});
+
